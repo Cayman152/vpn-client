3f05074bcc9e1a942749bb11b714d908544016ec
Никита Кожухов <n.kozhukhov@MacBook-Air--NK.local>
2026-02-27 22:17:41 +0300

    Remove Android client and build pipeline

--- GhostVPN.Android/app/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
-# Ghost VPN Android MVP: no custom ProGuard rules yet.
--- GhostVPN.Android/app/src/main/java/com/ghostvpn/android/data/VpnRuntimeModels.kt
+++
@@ -1,9 +0,0 @@
-package com.ghostvpn.android.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class VpnStartPayload(
-    val configuration: VpnConfiguration,
-    val routingPresets: RoutingPresets
-)
